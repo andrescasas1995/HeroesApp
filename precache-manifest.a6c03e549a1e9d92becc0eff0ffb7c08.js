0eaac95bb8964c258b81586def5ed7a02c4ee406
Andres Casas <walterandrescasas@hotmail.com>
2020-07-30 10:10:57 -0500

    Updates

---
+++ precache-manifest.a6c03e549a1e9d92becc0eff0ffb7c08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df37571683862556a8dba957a4d0717a",
+    "url": "/HeroesApp/index.html"
+  },
+  {
+    "revision": "7ec454a330af67d8deb8",
+    "url": "/HeroesApp/static/js/2.dc642471.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HeroesApp/static/js/2.dc642471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9cb8575f953c00cae2",
+    "url": "/HeroesApp/static/js/main.7663338d.chunk.js"
+  },
+  {
+    "revision": "860ec4534e9872461735",
+    "url": "/HeroesApp/static/js/runtime-main.6cc60b0b.js"
+  }
+]);
